76a5122b0da989a56df87084f7115242887fdf9e
ReinGonJin <ReinGonJin@gmail.com>
2025-01-27 05:21:48 -0500

    Update js.js
    Rgb split

--- js.js
+++ js.js
@@ -272,3 +272,25 @@ class PixelCanvas extends HTMLElement {
 }
 
 PixelCanvas.register();
+
+function channelSplit(elem) {
+  if (elem) {
+      const inner = elem.innerHTML;
+      elem.innerHTML = "";
+
+      const spanHidden = document.createElement("span");
+      spanHidden.classList.add("channel-split-static");
+      spanHidden.innerHTML = inner;
+      elem.appendChild(spanHidden);
+
+      ["red", "green", "blue"].forEach(x => {
+          const span = document.createElement("span");
+          span.classList.add("channel-split");
+          span.classList.add(`channel-split-${x}`);
+          span.innerHTML = inner;
+          elem.appendChild(span);
+      });
+  }
+}
+
+channelSplit(document.querySelector(".channel-split-wrapper"));
